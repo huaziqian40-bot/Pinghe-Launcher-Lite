b06bce8608cf8d0031c5eee29042920c9876bf28
hzq <huaziqian25@shph.com>
2026-09-25 18:58:25 +0800

    Bump version to 1.2.2 (first release shipping in-app auto-update)

--- installer/installer.py
+++ installer/installer.py
@@ -26,7 +26,7 @@ UNINSTALL_EXE = "Uninstall.exe"
 APP_NAME = "Pinghe Launcher Lite"
 REG_KEY = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\PingheLauncherLite"
 LNK_NAME = "Pinghe Launcher Lite.lnk"
-VERSION = "1.2.1"
+VERSION = "1.2.2"
 
 BUNDLED_APP = Path(getattr(sys, "_MEIPASS", ".")) / APP_EXE
 
